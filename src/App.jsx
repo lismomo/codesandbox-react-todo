ebb391d268fa9ac90c34eb618010c59912d2b707
lismomo <93498377+lismomo@users.noreply.github.com>
2021-11-07 20:53:50 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "./styles.css";
+
+export const App = () => {
+  const [incompleteTodos, setIncompleteTodos] = useState([
+    "ああああ",
+    "いいいいい"
+  ]);
+  return (
+    <>
+      <div className="input-area">
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          {incompleteTodos.map((todo) => {
+            return (
+              <li>
+                <div className="list-row">
+                  <p>{todo}</p>
+                  <button>完了</button>
+                  <button>削除</button>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
+        <ul>
+          <li>
+            <div className="list-row">
+              <p>うううう</p>
+              <button>戻す</button>
+            </div>
+          </li>
+          <li>
+            <div className="list-row">
+              <p>ええええ</p>
+              <button>戻す</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
